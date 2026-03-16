fa4d0bee351b1a35c61b82dbcc224f65f52b4d6e
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2026-03-16 15:37:55 -0300

    Primer commit de mi proyecto

--- api/src/main/java/com/dinastiaarg/api/config/CorsConfig.java
+++ api/src/main/java/com/dinastiaarg/api/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                // Agregamos tanto el localhost para pruebas como el dominio real de Vercel
+                // Aquí listamos EXACTAMENTE los dominios que pueden entrar
                 .allowedOrigins(
                         "http://localhost:5173",
                         "https://dinastiaarg.vercel.app"
